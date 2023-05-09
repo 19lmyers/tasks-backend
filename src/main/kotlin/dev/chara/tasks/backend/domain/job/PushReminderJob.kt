448376e0b5809f0020821066c0d85e3b05761174
Luke Myers <19lmyers@gmail.com>
2023-05-08 19:31:13 -0700

    Fix notifications for multiple devices

--- src/main/kotlin/dev/chara/tasks/backend/domain/job/PushReminderJob.kt
+++ src/main/kotlin/dev/chara/tasks/backend/domain/job/PushReminderJob.kt
@@ -2,9 +2,7 @@ package dev.chara.tasks.backend.v2.scheduler.job
 
 import com.github.michaelbull.result.mapBoth
 import com.github.michaelbull.result.mapError
-import com.google.firebase.messaging.FirebaseMessaging
-import com.google.firebase.messaging.MessagingErrorCode
-import com.google.firebase.messaging.MulticastMessage
+import com.google.firebase.messaging.*
 import dev.chara.tasks.backend.data.repository.FirebaseTokenRepository
 import dev.chara.tasks.backend.data.repository.ReminderRepository
 import io.ktor.util.logging.*
@@ -26,41 +24,43 @@ class PushReminderJob : CoroutineJob() {
 
         reminderRepository.get().mapBoth(
             success = { reminders ->
+                val messages = mutableListOf<Pair<String, Message>>();
+
                 for (reminder in reminders) {
                     logger.info("Dispatching reminder for task ${reminder.task_id}")
 
+                    val alert = ApsAlert.builder()
+                        .setTitle(reminder.task_label)
+                        .setBody(reminder.list_title)
+                        .build()
+
+                    val aps = Aps.builder()
+                        .setAlert(alert)
+                        .setCategory("reminder")
+                        .build()
+
+                    val apnsConfig = ApnsConfig.builder()
+                        .setAps(aps)
+                        .build()
+
                     firebaseTokenRepository.getForUser(reminder.user_id).mapBoth(
-                        success = { tokens ->
-                            val message = MulticastMessage.builder()
-                                .putData(DATA_MESSAGE_TYPE, MESSAGE_TYPE_REMINDER)
-                                .putData(DATA_TASK_ID, reminder.task_id)
-                                .putData(DATA_TASK_LABEL, reminder.task_label)
-                                .putData(DATA_LIST_TITLE, reminder.list_title)
-                                .putData(DATA_LIST_COLOR, reminder.list_color?.name ?: "")
-                                .putData(DATA_LIST_ICON, reminder.list_icon?.name ?: "")
-                                .addAllTokens(tokens)
-                                .build()
+                        success = { userTokens ->
+                            for (token in userTokens) {
+                                val message = Message.builder()
+                                    .putData(DATA_MESSAGE_TYPE, MESSAGE_TYPE_REMINDER)
+                                    .putData(DATA_TASK_ID, reminder.task_id)
+                                    .putData(DATA_TASK_LABEL, reminder.task_label)
+                                    .putData(DATA_LIST_TITLE, reminder.list_title)
+                                    .putData(DATA_LIST_COLOR, reminder.list_color?.name ?: "")
+                                    .putData(DATA_LIST_ICON, reminder.list_icon?.name ?: "")
+                                    .setApnsConfig(apnsConfig)
+                                    .setToken(token)
+                                    .build()
 
-                            reminderRepository.setFired(reminder.user_id, reminder.task_id).mapError {
-                                logger.error(it.throwable)
-                            }
+                                messages.add(token to message)
 
-                            val batchResponse = FirebaseMessaging.getInstance().sendMulticast(message)
-
-                            if (batchResponse.failureCount > 0) {
-                                for ((index, response) in batchResponse.responses.withIndex()) {
-                                    if (!response.isSuccessful) {
-                                        val errorCode = response.exception.messagingErrorCode
-
-                                        logger.warn("Error code: {}", errorCode)
-
-                                        if (errorCode == MessagingErrorCode.UNREGISTERED || errorCode == MessagingErrorCode.INVALID_ARGUMENT) {
-                                            val failedToken = tokens[index]
-                                            firebaseTokenRepository.invalidate(failedToken).mapError {
-                                                logger.error(it.throwable)
-                                            }
-                                        }
-                                    }
+                                reminderRepository.setFired(reminder.user_id, reminder.task_id).mapError {
+                                    logger.error(it.throwable)
                                 }
                             }
                         },
@@ -68,6 +68,27 @@ class PushReminderJob : CoroutineJob() {
                             logger.error(it.throwable)
                         }
                     )
+                }
+
+                for (batch in messages.chunked(500)) {
+                    val batchResponse = FirebaseMessaging.getInstance().sendAll(batch.map { it.second })
+
+                    if (batchResponse.failureCount > 0) {
+                        for ((index, response) in batchResponse.responses.withIndex()) {
+                            if (!response.isSuccessful) {
+                                val errorCode = response.exception.messagingErrorCode
+
+                                logger.warn("Error code: {}", errorCode)
+
+                                if (errorCode == MessagingErrorCode.UNREGISTERED || errorCode == MessagingErrorCode.INVALID_ARGUMENT) {
+                                    val failedToken = batch[index].first
+                                    firebaseTokenRepository.invalidate(failedToken).mapError {
+                                        logger.error(it.throwable)
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             },
             failure = {
